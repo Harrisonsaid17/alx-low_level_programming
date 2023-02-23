99930dcd6494ac250c81312e8f1c1c30d66a5aa5
Harrisonsaid17 <kamandasaid17@gmail.com>
2023-02-23 09:25:43 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+		while (i <= 99)
+		{
+			j = (i / 10 + '0');
+			k = (i % 10 + '0');
+
+		if (j < k)
+		{
+			putchar(j);
+			putchar(k);
+
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
